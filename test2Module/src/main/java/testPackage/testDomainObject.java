e0d403e653c59b04c192e3eaa127971b4aa7896c
Nate Hagan <nahag@microsoft.com>
2016-11-04 15:24:09 -0700

    Added test Java module

---
+++ test2Module/src/main/java/testPackage/testDomainObject.java
@@ -0,0 +1,13 @@
+package testPackage;
+
+/**
+ * Created by nahag on 11/4/2016.
+ */
+public class testDomainObject {
+    public Integer I;
+    public Double JDouble;
+
+    public testDomainObject() {
+
+    }
+}
